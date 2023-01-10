0ca32ba0424ee66939866e2c315759a654d8c3dc
Rahmad Adi Pratama <OhMyVenyx@proton.me>
2023-01-10 12:35:41 +0700

    ci: Qassa ysl
    Signed-off-by: Rahmad Adi Pratama <OhMyVenyx@proton.me>

---
+++ collect.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ~/rom
+
+. build/envsetup.sh
+export TZ=Asia/Jakarta
+export CCACHE_DIR=~/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 100G -F 0
+ccache -o compression=true
+ccache -z
+$lunch
+$make -j8 &
+sleep 100m
+kill %1
+ccache -s
+
---
+++ final.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ~/rom
+
+. build/envsetup.sh
+export TZ=Asia/Jakarta
+export CCACHE_DIR=~/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 100G -F 0
+ccache -o compression=true
+ccache -z
+
+$lunch
+$make -j8
+
