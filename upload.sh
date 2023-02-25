945a4497b297770881a500827a2ae06b76c7f5f5
rhmd19801 <122339284+rhmd19801@users.noreply.github.com>
2023-02-25 18:49:24 +0700

    ci: Upload kernel image too

--- upload.sh
+++ upload.sh
@@ -3,3 +3,7 @@ if [ -f ~/rom/out/target/product/juice/crDroidAndroid*.zip ]; then
       rclone copy ~/rom/out/target/product/juice/crDroidAndroid*.zip venyx:juice -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls crDroidAndroid*.zip) Uploaded Successfully!"
 fi
+
+if [ -f ~/rom/out/target/product/juice/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz ]; then
+      rclone copy ~/rom/out/target/product/juice/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz venyx:juice -P
+fi
