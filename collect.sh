feb0a3bbb462bce23376eba599ac785fbb94c50c
rhmd19801 <rhmd.19801@gmail.com>
2023-02-27 23:38:58 +0700

    ci: init xdCAF juice
    Signed-off-by: rhmd19801 <rhmd.19801@gmail.com>

--- collect.sh
+++ collect.sh
@@ -6,6 +6,8 @@ export TZ=Asia/Jakarta
 export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
+export BUILD_USERNAME=rahmad
+export BUILD_HOSTNAME=IDN-Labs
 ccache -M 100G -F 0
 ccache -o compression=true
 ccache -z
--- upload.sh
+++ upload.sh
@@ -1,9 +1,5 @@
-if [ -f ~/rom/out/target/product/juice/crDroidAndroid*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls crDroidAndroid*.zip)"
-      rclone copy ~/rom/out/target/product/juice/crDroidAndroid*.zip venyx:juice -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls crDroidAndroid*.zip) Uploaded Successfully!"
-fi
-
-if [ -f ~/rom/out/target/product/juice/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz ]; then
-      rclone copy ~/rom/out/target/product/juice/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz venyx:juice -P
+if [ -f ~/rom/out/target/product/juice/xdCAF*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls xdCAF*.zip)"
+      rclone copy ~/rom/out/target/product/juice/xdCAF*.zip venyx:juice -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls xdCAF*.zip) Uploaded Successfully!"
 fi
