6226e9d152764bf19d77dc263926bd58ac95fd74
rhmd19801 <rhmd.19801@gmail.com>
2023-02-28 19:42:33 +0700

    ci: Upload logs if found any error
    Signed-off-by: rhmd19801 <rhmd.19801@gmail.com>

--- upload.sh
+++ upload.sh
@@ -3,3 +3,12 @@ if [ -f ~/rom/out/target/product/juice/xdCAF*.zip ]; then
       rclone copy ~/rom/out/target/product/juice/xdCAF*.zip venyx:juice -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls xdCAF*.zip) Uploaded Successfully!"
 fi
+
+if [ -f ~/rom/out/target/product/juice/error.log ]; then
+    sudo apt update && sudo apt install curl -y
+    curl -F document=@~/rom/out/target/product/juice/error.log https://api.telegram.org/bot$tokentl/sendDocument \
+        -F chat_id=$idtl \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Check these logs"
+fi
