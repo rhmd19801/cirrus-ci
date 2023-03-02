4bcfcb29418ba1477b800d82b88ecfbcb5c38cb2
rhmd19801 <rhmd.19801@gmail.com>
2023-03-02 12:07:08 +0700

    ci: Skip checkapi
    Signed-off-by: rhmd19801 <rhmd.19801@gmail.com>

--- final.sh
+++ final.sh
@@ -6,6 +6,7 @@ export TZ=Asia/Jakarta
 export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
+export WITHOUT_CHECK_API=true
 export BUILD_USERNAME=rahmad
 export BUILD_HOSTNAME=IDN-Labs
 ccache -M 100G -F 0
