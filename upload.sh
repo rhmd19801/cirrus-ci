7c9ee11da0dfbe856ad47f5418eed3b3fabe2723
rhmd19801 <rhmd.19801@gmail.com>
2023-04-07 09:09:30 +0700

    ci: Init Qassa Juice

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/ysl/qassa-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ysl/ && ls qassa-*.zip)"
-      rclone copy ~/rom/out/target/product/ysl/qassa-*.zip venyx:ysl -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls qassa-*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/juice/qassa-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls qassa-*.zip)"
+      rclone copy ~/rom/out/target/product/juice/qassa-*.zip venyx:juice -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls qassa-*.zip) Uploaded Successfully!"
 fi
