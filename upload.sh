656325773844f414ebd5e52d079a3b5ea392dd12
rhmd19801 <rhmd.19801@gmail.com>
2023-06-27 02:48:53 +0700

    ci: init xdCLO whyred

--- upload.sh
+++ upload.sh
@@ -1,11 +1,11 @@
-if [ -f ~/rom/out/target/product/juice/xdCAF*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls xdCAF*.zip)"
-      rclone copy ~/rom/out/target/product/juice/xdCAF*.zip venyx:juice -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls xdCAF*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/whyred/xdCLO*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/whyred/ && ls xdCLO*.zip)"
+      rclone copy ~/rom/out/target/product/whyred/xdCLO*.zip venyx:whyred -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/whyred/ && ls xdCLO*.zip) Uploaded Successfully!"
 fi
 
-if [ -f ~/rom/out/target/product/juice/error.log ]; then
-      LOG=~/rom/out/target/product/juice/error.log
+if [ -f ~/rom/out/target/product/whyred/error.log ]; then
+      LOG=~/rom/out/error.log
       curl -F document=@$LOG https://api.telegram.org/$tokentl/sendDocument?chat_id=$idtl \
       -F caption="Check these error logs"
 fi
