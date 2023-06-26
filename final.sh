77fc2b6d5b2a783e6d535cc35202add916dc1c72
rhmd19801 <rhmd.19801@gmail.com>
2023-06-27 03:02:06 +0700

    ci: init xdCLO whyred

--- final.sh
+++ final.sh
@@ -6,9 +6,10 @@ export TZ=Asia/Jakarta
 export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-export WITHOUT_CHECK_API=true
-export BUILD_USERNAME=rahmad
-export BUILD_HOSTNAME=IDN-Labs
+export BUILD_USERNAME="OhMyVenyx"
+export BUILD_HOSTNAME="IDN-Labs"
+export KBUILD_BUILD_USER="Rahmad Adi Pratama"
+export KBUILD_BUILD_HOST="OhMyVenyx"
 ccache -M 100G -F 0
 ccache -o compression=true
 ccache -z
