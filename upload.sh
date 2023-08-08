16bd277d4d1be5dc098c954383272f8f5563b397
rhmd19801 <rhmd.19801@gmail.com>
2023-08-08 18:07:07 +0700

    ci: Init xdroid-CLO ysl
    Signed-off-by: rhmd19801 <rhmd.19801@gmail.com>

--- upload.sh
+++ upload.sh
@@ -1,11 +1,11 @@
-if [ -f ~/rom/out/target/product/whyred/xdCLO*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/whyred/ && ls xdCLO*.zip)"
-      rclone copy ~/rom/out/target/product/whyred/xdCLO*.zip venyx:whyred -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/whyred/ && ls xdCLO*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/ysl/xdCLO*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ysl/ && ls xdCLO*.zip)"
+      rclone copy ~/rom/out/target/product/ysl/xdCLO*.zip venyx:ysl -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls xdCLO*.zip) Uploaded Successfully!"
 fi
 
-if [ -f ~/rom/out/target/product/whyred/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb ]; then
-      rclone copy ~/rom/out/target/product/whyred/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb venyx:whyred -P
+if [ -f ~/rom/out/target/product/ysl/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb ]; then
+      rclone copy ~/rom/out/target/product/ysl/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb venyx:ysl -P
 fi
 
 if [ -f ~/rom/out/error.log ]; then
