ecf2c9acfa9ba4bc1a5068a60230c228ae51ce8b
rhmd19801 <122339284+rhmd19801@users.noreply.github.com>
2023-08-26 05:23:47 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -3,3 +3,7 @@ if [ -f ~/rom/out/target/product/ysl/qassa-*.zip ]; then
       rclone copy ~/rom/out/target/product/ysl/qassa-*.zip venyx:ysl -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls qassa-*.zip) Uploaded Successfully!"
 fi
+
+if [ -f ~/rom/out/target/product/ysl/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb ]; then
+      rclone copy ~/rom/out/target/product/ysl/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb venyx:ysl -P
+fi
