77de8616852ff5fd5acfb77c26c6ef58aefd26e1
rhmd19801 <122339284+rhmd19801@users.noreply.github.com>
2023-09-18 03:04:44 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
-if [ -f ~/rom/out/target/product/ysl/qassa-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ysl/ && ls qassa-*.zip)"
-      rclone copy ~/rom/out/target/product/ysl/qassa-*.zip venyx:ysl -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls qassa-*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/ysl/qassa*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ysl/ && ls qassa*.zip)"
+      rclone copy ~/rom/out/target/product/ysl/qassa*.zip venyx:ysl -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls qassa*.zip) Uploaded Successfully!"
 fi
 
 if [ -f ~/rom/out/target/product/ysl/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb ]; then
