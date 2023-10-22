5d52b52ffc63f1b7e7a92794bc300053361405d5
rhmd19801 <122339284+rhmd19801@users.noreply.github.com>
2023-10-22 09:20:27 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
-if [ -f ~/rom/out/target/product/ysl/xdCLO*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ysl/ && ls xdCLO*.zip)"
-      rclone copy ~/rom/out/target/product/ysl/xdCLO*.zip venyx:ysl -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls xdCLO*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/ysl/Kasumi*ysl*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ysl/ && ls Kasumi*.zip)"
+      rclone copy ~/rom/out/target/product/ysl/Kasumi*ysl*.zip venyx:ysl -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls Kasumi*.zip) Uploaded Successfully!"
 fi
 
 if [ -f ~/rom/out/target/product/ysl/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb ]; then
