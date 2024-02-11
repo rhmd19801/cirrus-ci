9b98f97260e75592d77544c35760a0622d74f793
rhmd19801 <122339284+rhmd19801@users.noreply.github.com>
2024-02-11 18:23:32 +0700

    salah memk

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-if [ -f ~/rom/out/target/product/yal/qassa*.zip ]; then
+if [ -f ~/rom/out/target/product/ysl/qassa*.zip ]; then
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ysl/ && ls qassa*.zip)"
       rclone copy ~/rom/out/target/product/ysl/qassa*.zip venyx:citrus -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls qassa*.zip) Uploaded Successfully!"
